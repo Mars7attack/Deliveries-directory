dfda89cd5bda7d84941087da95964e41da18b765
Mars7attack <antoine.dufetelle@epitech.digital>
2024-09-24 15:38:40 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,2 +1,9 @@
-document.write("Welcome to the javascript pool! <br> Have some fun and dont forget to bring a towel.")
+// HTML : 
+
+<h3>
+Welcome to the javascript pool! <br> Have some fun and dont forget to bring a towel.
+</h3>
+
+// JavaScript : 
+
 console.log("Website launched!")
